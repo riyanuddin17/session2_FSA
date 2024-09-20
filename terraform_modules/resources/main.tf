af84e3f93216f425bb7cc8b04916b22200085457
Riyan Uddin <107648620+riyanuddin17@users.noreply.github.com>
2024-09-21 00:41:09 +0530

    Update main.tf

--- terraform_modules/resources/main.tf
+++ terraform_modules/resources/main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   source = "./VPC"
 }
 module "aws_instance_1" {
-  source                      = "./EC2"
+  source                      = "../modules/EC2"
   ami                         = var.ami
   subnet_id                   = module.vpc.pub_sub1
   vpc_security_group_ids      = [module.vpc.webSg]
@@ -11,7 +11,7 @@ module "aws_instance_1" {
 
 }
 module "aws_instance_2" {
-  source                      = "./EC2"
+  source                      = "../modules/EC2"
   ami                         = var.ami
   subnet_id                   = module.vpc.pvt_sub1
   vpc_security_group_ids      = [module.vpc.webSg]
